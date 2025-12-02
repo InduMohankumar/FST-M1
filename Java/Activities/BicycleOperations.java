3b02c3b883a07349c187d6d0a46b4fe231bed239
InduMohankumar <Indu.Mohankumar@ibm.com>
2025-12-02 10:29:21 +0530

    Add files via upload

---
+++ Java/Activities/BicycleOperations.java
@@ -0,0 +1,6 @@
+package day1;
+
+interface BicycleOperations {
+    public void applyBrake(int decrement);
+    public void speedUp(int increment);
+}
---
+++ Java/Activities/BicycleParts.java
@@ -0,0 +1,6 @@
+package day1;
+
+interface BicycleParts {
+    public int tyres = 2;
+    public int maxSpeed = 25;
+}
