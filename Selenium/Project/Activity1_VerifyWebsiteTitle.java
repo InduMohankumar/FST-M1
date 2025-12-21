412fcf762d586a50fe2abd8a2717f77c6de25c67
InduMohankumar <Indu.Mohankumar@ibm.com>
2025-12-21 11:29:52 +0530

    Add files via upload

--- Selenium/Project/Activity1_VerifyWebsiteTitle.java
+++ Selenium/Project/Activity1_VerifyWebsiteTitle.java
@@ -1,23 +1,24 @@
 package activities;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Activity1_VerifyWebsiteTitle {
-	
+
 	@Test
-	 public void verifyWebsiteTitle() {
-		 WebDriver driver=new FirefoxDriver();
-		 driver.manage().window().maximize();
-		 
-		 driver.get("http://alchemy.hguy.co/orangehrm");
-		 String actualTitle=driver.getTitle();
-		 String expectedTitle = "OrangeHRM";
-		 Assert.assertEquals(actualTitle, expectedTitle);
-         driver.quit();
-		 
-	 }
-	
+	public void verifyWebsiteTitle() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+
+		driver.get("http://alchemy.hguy.co/orangehrm");
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "OrangeHRM";
+		Assert.assertEquals(actualTitle, expectedTitle);
+		System.out.println("Title:" + actualTitle);
+		driver.quit();
+
+	}
 
 }
